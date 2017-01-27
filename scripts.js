b5a54b18f41cb2e720dcfda351b765a792204063
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-01-27 17:07:50 -0500

    click a letter, it either adds to word or to incorrect

--- scripts.js
+++ scripts.js
@@ -3,12 +3,13 @@
 function Game(gameString) {
 	this.answerArray = gameString.split(""); //make an answerArray
 	this.playerArray = this.answerArray.map(function(element) { return "?"})
-	this.optionsArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	// this.optionsArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 	this.wrongArray = [];
 	this.gameboard = new Board(this.answerArray);
 	this.gameboard.initialize()
 
 	this.playLetter = function(letter) {
+		// this.optionsArray.splice(this.optionsArray.indexOf(letter),1);
 		if (this.answerArray.includes(letter)) {
 			var indices = []
 			var idx = this.answerArray.indexOf(letter);
@@ -24,11 +25,12 @@ function Game(gameString) {
 			}
 		} else {
 			this.wrongArray.push(letter)
+			updateIncorrect(letter)
 		}
 	}
 
 	this.playLetter(" ")
-	console.log(this.playerArray)
+	// console.log(this.playerArray)
 }
 
 function Board(answerArray) {
@@ -51,4 +53,27 @@ function Board(answerArray) {
 	
 }
 
-var myGame = new Game("hello darkness my old friend")
+var myGame = new Game("i heard words i never heard in the bible")
+var letterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+function makeLetterArray() {
+	var htmlLetterString = ""
+	for (var i=0; i<letterArray.length; i++) {
+		htmlLetterString += "<div class='letter' id='guess-letter-" + letterArray[i] + "'>" + letterArray[i] + "</div>"
+	}
+	$("#options").html(htmlLetterString)
+
+	$(".letter").click(function() {
+		var clickedLetter = $( event.target ).text()
+		myGame.playLetter(clickedLetter);
+		$("#guess-letter-"+clickedLetter).hide();
+	})
+}
+
+function updateIncorrect(letter) {
+	htmlLetterString = "<div class='letter'>" + letter + "</div>"
+	$("#guesses").append(htmlLetterString);
+}
+
+makeLetterArray();
+
